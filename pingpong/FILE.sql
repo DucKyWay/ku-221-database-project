8dca5e2e376602a2b4341b9b98c73faa2426a154
DucKyWay <niji2549@gmail.com>
2026-03-07 22:07:11 +0700

    add phpMyAdmin config

--- pingpong/FILE.sql
+++ pingpong/FILE.sql
@@ -1,3 +1,7 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 create table `FILE` (
     `file_id` int(11) not null auto_increment,
     `external_key` varchar(255) not null,
@@ -7,4 +11,4 @@ create table `FILE` (
     `metadata` varchar(255),
     `file_created_at` datetime default current_timestamp,
     primary key (`file_id`)
-)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
--- pingpong/SHOP_UPLOAD.sql
+++ pingpong/SHOP_UPLOAD.sql
@@ -1,3 +1,7 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 create table `SHOP_UPLOAD` (
     `shop_id` int(11) not null,
     `file_id` int(11) not null,
@@ -5,3 +9,4 @@ create table `SHOP_UPLOAD` (
     foreign key (`shop_id`) references `SHOP`(`shop_id`),
     foreign key (`file_id`) references `FILE`(`file_id`)
 )
+ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
