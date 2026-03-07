5b1ad91089b1d56f759a3a677063d7ca34a02a1c
phai kung <wachirawichsuchaiyasit@gmail.com>
2026-03-07 23:49:23 +0700

    67

--- pingpong/SHOP_UPLOAD.sql
+++ pingpong/SHOP_UPLOAD.sql
@@ -3,10 +3,10 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 create table `SHOP_UPLOAD` (
-    `shop_id` int(11) not null,
+    `shop_name` int(11) not null,
     `file_id` int(11) not null,
     `uploaded_at` datetime not null,
-    foreign key (`shop_id`) references `SHOP`(`shop_id`),
+    foreign key (`shop_name`) references `SHOP`(`shop_name`),
     foreign key (`file_id`) references `FILE`(`file_id`)
 )
 ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
